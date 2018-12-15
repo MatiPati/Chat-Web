5e7f434df162937383fd677bf6db8e038c03aae7
Mateusz Ożóg <mateusz2345678@gmail.com>
2018-12-15 02:02:59 +0100

    home api showing _

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2>Strona główna</h2>
-    <p>
-        Odpowiedź od api (azurix.pl:8080)<br><br>
-        <span id="api-res"></span>
-    </p>
-</div>
+    <div class="container">
+        <h1 class="mb-5">Strona główna</h1>
+        <div class="jumbotron">
+            <p class="mb-1">
+                Odpowiedź od api (azurix.pl:8080):
+            </p>
+            <p id="api-res" class="h2"></p>
+        </div>
+    </div>
 @endsection

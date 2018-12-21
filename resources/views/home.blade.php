e7e9996b25be7510d525b95336aba8c24733f037
Mateusz Ożóg <mateusz2345678@gmail.com>
2018-12-21 17:51:09 +0100

    Home template v1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.default')
 
 @section('content')
+    @include('inc.navbar')
 
-    <h1>Home</h1>
+    <div id="home-slider">
+        <div class="slider-text">
+            <h1>Ch-APP</h1>
+            <p>
+                The world's first Chatting-APP which uses Ch-API
+            </p>
+            <div class="mt-5">
+                <a href="/register" class="btn btn-primary">
+                    <i class='bx bx-user-plus'></i> Register account
+                </a>
+                <a href="/register" class="btn btn-primary">
+                    <i class='bx bx-user-check'></i> Log-in
+                </a>
+            </div>
+        </div>
+        <div class="slider-img"></div>
+        <div class="github p-4">
+            <a href="#" target="_blank"><i class="bx bxl-github h1 m-0"></i></a>
+            <p class="mt-3 mb-0 small">Powered by <a href="https://azurix.pl" target="_blank">azurix.pl</a></p>
+        </div>
+    </div>
+
+
 
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,12 +3,8 @@
     @include('inc.head')
 </head>
 <body>
-    @include('inc.navbar')
-    <main class="my-5">
-        <div class="container-fluid">
-            @include('inc.messages')
-            @yield('content')
-        </div>
-    </main>
+<main>
+    @yield('content')
+</main>
 </body>
 </html>
